bec5e5081bfff524c852408165046d6bb33a17a3
benaarrouch <benaarrouch.mohamed01@gmail.com>
2023-05-03 23:08:14 +0100

    the new version

--- src/main/java/com/example/spring2key/spring2key/Controller/SecuredController.java
+++ src/main/java/com/example/spring2key/spring2key/Controller/SecuredController.java
@@ -1,18 +1,30 @@
 package com.example.spring2key.spring2key.Controller;
 
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @Controller
 public class SecuredController {
 
+    @GetMapping("/")
+    public String notSecured(){
+       return "notSecured";
+    }
     //secured(): This endpoint maps to the GET request for the /secured path.
     // It returns the template "secured.html"
     @GetMapping("/secured")
-    public String secured() {
+    public String secured(Principal principal, Model model) {
+        KeycloakAuthenticationToken token = (KeycloakAuthenticationToken) principal;
+        AccessToken accessToken = token.getAccount().getKeycloakSecurityContext().getToken();
+        String username = accessToken.getPreferredUsername();
+        model.addAttribute("username",username);
         return "secured";
     }
 
